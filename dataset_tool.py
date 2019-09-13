dbfd50ce4920d808ef4b8e5b63a5b766105b8b94
Miquel Larsson <miquellarsson@gmail.com>
2019-09-14 01:48:59 +0200

    Avoid error with dataset_tool

--- dataset_tool.py
+++ dataset_tool.py
@@ -523,7 +523,10 @@ def create_from_images(tfrecord_dir, image_dir, shuffle):
             if channels == 1:
                 img = img[np.newaxis, :, :] # HW => CHW
             else:
-                img = img.transpose([2, 0, 1]) # HWC => CHW
+                try:
+                    img = img.transpose([2, 0, 1]) # HWC => CHW
+                except:
+                    pass
             tfr.add_image(img)
 
 #----------------------------------------------------------------------------
